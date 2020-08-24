53edc34c44397c7444191f5051c3f18b6a609563
sebastian <jsh7590cr@gmail.com>
2020-08-24 17:22:22 -0500

    Revert "Revert "Agregado registro al modulo Admin del modelo DatosComentarios""
    This reverts commit 8149317e36ac708c8a26b5dd17bb221ffd722ed7.

--- CuentasFalsas/CuentasFalsas/settings.py
+++ CuentasFalsas/CuentasFalsas/settings.py
@@ -32,12 +32,12 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'django.contrib.admin',
+    'DatosComentarios',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'DatosComentarios',
     'rest_framework',
     'corsheaders',
 ]
