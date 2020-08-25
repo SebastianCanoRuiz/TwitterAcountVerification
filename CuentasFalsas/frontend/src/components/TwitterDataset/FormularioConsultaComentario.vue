595fb87a49da2a3cdad7851f43b549d35101e128
sebastian <jsh7590cr@gmail.com>
2020-08-25 00:05:00 -0500

    Creado vista principal para consultar la veracidad de una cuenta de twitter mediante sus comentarios

--- CuentasFalsas/frontend/src/components/TwitterDataset/FormularioConsultaComentario.vue
+++ CuentasFalsas/frontend/src/components/TwitterDataset/FormularioConsultaComentario.vue
@@ -2,9 +2,38 @@
     <div class="container">
         <div class="row">
             <div class="col text-left">
-                <h2>Ingrese un Comentario</h2>
+                <h2>Consultar veracidad cuenta de Twitter basado en un comentario</h2>
             </div>
         </div>
+
+        <div class="row">
+            <div class="col">
+                <div class="card">
+                    <div class="card-body">
+
+                        <form @onSubmit="onSubmit">
+
+                            <div class="form-group row">
+                                <label for="title" class="col-sm-2 col-form-label">Comentario</label>
+                                <div class="col-sm-6">
+                                    <textarea name="comentario" class="form-control" placeholder="Ingrese un comentario..." rows="3"></textarea>
+                                </div>
+                            </div>
+
+                            <div class="rows">
+                                <div class="col text-left">
+                                    <b-button type="submit" variant="primary">Consultar</b-button>
+                                </div>
+                            </div>
+                        
+                        </form>
+                    
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
     </div>
 
         
@@ -13,11 +42,8 @@
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style>
-
 </style>
