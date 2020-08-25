385099aed12c99adb61742ab50dfa9f5d2d201fe
sebastian <jsh7590cr@gmail.com>
2020-08-24 23:38:49 -0500

    Componente nuevo, consulta de comentario para determinar el tipo de la cuenta --Construcción

---
+++ CuentasFalsas/frontend/src/components/TwitterDataset/FormularioConsultaComentario.vue
@@ -0,0 +1,23 @@
+<template lang="html">
+    <div class="container">
+        <div class="row">
+            <div class="col text-left">
+                <h2>Ingrese un Comentario</h2>
+            </div>
+        </div>
+    </div>
+
+        
+    </div>
+  
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
