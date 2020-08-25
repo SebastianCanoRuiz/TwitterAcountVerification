3cab0baacb1abf03e4f361190f5b2f53d97da0ec
sebastian <jsh7590cr@gmail.com>
2020-08-24 21:18:15 -0500

    Agregado módulo Frontend Vue.JS

--- CuentasFalsas/DatosComentarios/urls.py
+++ CuentasFalsas/DatosComentarios/urls.py
@@ -1,8 +1,11 @@
 from rest_framework import routers
-
+from django.conf.urls import url
 from .viewsets import DatosComentariosViewSet
 
 router = routers.SimpleRouter()
 router.register('DatosComentarios', DatosComentariosViewSet)
 
-urlpatterns = router.urls
+urlpatterns = [
+    path('coment/', DatosComentariosViewSet),
+]
+#urlpatterns = router.urls
